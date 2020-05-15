905462e5ea04c8024e241673d547ccca636f68fb
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-05-16 01:31:11 +0530

    [Rupesh Patil].Updated Cart Page Design

--- src/app/shared/model/costomer.model.ts
+++ src/app/shared/model/costomer.model.ts
@@ -1,9 +1,11 @@
 export class Costomer {
   customerName: string;
+  addressId: string;
   contact: string;
   pinCode: string;
   locality: string;
   city: string;
+  country: string;
   landMark: string;
   address: string;
   type: string;
