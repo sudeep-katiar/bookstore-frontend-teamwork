8cac25b2c137122de1fb727409edabe4124ad6f9
durgasankar <durgasankar.raja500@gmail.com>
2020-06-09 17:35:24 +0530

    [durgasankar].add -> ratings page added and provide rating to the individual placed order

--- src/app/component/orders/orders.component.ts
+++ src/app/component/orders/orders.component.ts
@@ -1,4 +1,7 @@
+import { PlacedOrder } from "./../../shared/model/placed-order.model";
 import { Component, OnInit } from "@angular/core";
+import { CartServiceService } from "src/app/shared/service/cart-service.service";
+import { MatSnackBar } from "@angular/material";
 
 @Component({
   selector: "app-orders",
@@ -6,9 +9,30 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./orders.component.scss"],
 })
 export class OrdersComponent implements OnInit {
-  rating = 2.5;
-
-  constructor() {}
-
-  ngOnInit() {}
+  rating: number;
+  placedOrders: PlacedOrder[];
+  isRated: boolean = false;
+  constructor(
+    private cartService: CartServiceService,
+    private matSnackBar: MatSnackBar
+  ) {}
+  ngOnInit() {
+    this.cartService.autoRefresh$.subscribe(() => this.getAllOrderedBooks());
+    this.getAllOrderedBooks();
+  }
+  /**
+   * name
+   */
+  public getAllOrderedBooks() {
+    return this.cartService.getUsersOrdersList().subscribe(
+      (response: any) => {
+        console.log("response list : ", response);
+        this.placedOrders = response.orders;
+        console.log("placed orders : ", this.placedOrders);
+      },
+      (errors: any) => {
+        console.log("errors : ", errors);
+      }
+    );
+  }
 }
---
+++ src/app/shared/model/placed-order.model.ts
@@ -0,0 +1,8 @@
+export class PlacedOrder {
+  public authorName: string;
+  public bookImage: string;
+  public quantity: number;
+  public price: number;
+  public rating: number;
+  public bookName: string;
+}
